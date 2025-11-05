ea8e79f202172033c2bdc515855a68eeb0ee0ed5
PaulPayumo-design <jpogi3481@gmail.com>
2025-11-05 11:45:02 +0800

    updated ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import PostList from './components/PostList.jsx';
 import PostForm from './components/PostForm.jsx';
 
-const API_URL = "https://act-5-backend.onrender.com"; 
+const API_URL = "https://act-5-backend.onrender.com/api/posts"; 
 export default function App() {
   const [posts, setPosts] = useState([]);
   const [editing, setEditing] = useState(null);
